b8e7bfa92627c41fba6645893b56d46917b7b9c3
Ruby Chang <rchang825@hotmail.com>
2025-08-26 16:06:23 -0700

    fix packages and test retrieval from db

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ const db = require('./db');
 const app = express();
 
 
+app.get('/', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM reviews LIMIT 2');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log('Server is running on port', process.env.PORT);
 });
