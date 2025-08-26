da4998a6b85b57ed268617d04bd63e9891987970
Ruby Chang <rchang825@hotmail.com>
2025-08-26 16:00:48 -0700

    initialize index.js

--- server/index.js
+++ server/index.js
@@ -5,16 +5,6 @@ const db = require('./db');
 const app = express();
 
 
-app.get('/', async (req, res) => {
-  try {
-    const result = await db.query('SELECT * FROM reviews');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
 app.listen(process.env.PORT, () => {
   console.log('Server is running on port', process.env.PORT);
 });
