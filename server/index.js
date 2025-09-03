7a8445d206523deb547d40a7267121defdf57691
Ruby Chang <rchang825@hotmail.com>
2025-09-03 09:56:29 -0700

    loaderio config

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const router = require('./routes');
+const path = require('path');
 
 const app = express();
 
+app.use('/loaderio-ada5397f8066e6f74bfeca25f781179e.txt', express.static(path.join(__dirname, '/tests', 'loaderio.txt')));
 app.use(express.json());
 app.use('/', router);
 
--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,14 @@
 const controllers = require('./controllers');
 const router = require('express').Router();
+const path = require('path');
 
 router.get('/', async (req, res) => {
   res.send('routes: /reviews, reviews/meta');
 });
 
+router.get('/loaderio-ada5397f8066e6f74bfeca25f781179e.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '/tests', 'loaderio.txt'));
+});
 router.get('/reviews', controllers.getReviews);
 router.get('/reviews/meta', controllers.getMeta);
 router.post('/reviews', controllers.postReview);
